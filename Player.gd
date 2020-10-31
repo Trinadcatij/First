971d84805fd9a3cb8cf95cf718b178da2c664923
KIT_07_206 <KIT_07_206@stud.kai.ru>
2020-10-31 19:57:38 +0300

    jump demo added

--- Player.gd
+++ Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
 var speed = 45
-var jump_forse = 100
-var gravity=500
+var jump_forse = 50
+var gravity=600
 
 var vel = Vector2()
 
@@ -11,4 +11,7 @@ func _physics_process(delta):
 		vel.x-=speed
 	elif Input .is_action_pressed("player_right"):
 			vel.x+=speed
+	vel.y+=gravity*delta
+	if Input.is_action_pressed("player_jump"):
+		vel.y-=jump_forse
 	move_and_slide(vel,Vector2.UP)
