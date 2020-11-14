28491192c5b5c6e52ae53a06e3be38a06ff0ea05
KIT_07_206 <KIT_07_206@stud.kai.ru>
2020-11-14 18:35:07 +0300

    collision and flip added

--- Player.gd
+++ Player.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
-var speed = 45
-var jump_forse = 50
+var speed = 50
+var jump_forse = 300
 var gravity=600
 
 var vel = Vector2()
+
+onready var player_sprite = get_node("Player")
+
 
 func _physics_process(delta):
 	if Input.is_action_pressed("player_left"):
@@ -12,6 +15,10 @@ func _physics_process(delta):
 	elif Input .is_action_pressed("player_right"):
 			vel.x+=speed
 	vel.y+=gravity*delta
-	if Input.is_action_pressed("player_jump"):
-		vel.y-=jump_forse
-	move_and_slide(vel,Vector2.UP)
+	if Input.is_action_pressed("player_jump") and is_on_floor():
+		vel.y-=jump_forse 
+	vel = move_and_slide(vel,Vector2.UP)
+	if vel.x < 0 :
+		player_sprite.flip_h=true
+	if vel.x > 0 :
+		player_sprite.flip_h=false
